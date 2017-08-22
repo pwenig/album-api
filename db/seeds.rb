5670533cb6ec571e6318e6bf23d213c98a76144a
Paul Wenig <pwenig@gmail.com>
2017-08-22 13:29:34 -0600

    Seeds and csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,24 @@
-Artist.create({ name: 'Bruce Springsteen'})
-Artist.create({ name: 'U2'})
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'albums.csv')).scrub
+csv = CSV.parse(csv_text, :headers => true)
+artists = []
+
+puts "Creating artists...."
+# Create the artists first. Push the artists into an array so we can check for duplicats before creating
+csv.each do |row|
+  if artists.exclude?(row['artist'])
+    Artist.create!(name: row['artist'])
+    artists.push(row['artist'])
+  end
+  
+end
+
+puts "Creating albums..."
+# Create the albums
+csv.each do |row|
+  artist = Artist.find_by(name: row['artist'])
+  artist.albums.create!(album: row['album'], genre: row['genre'], year: row['year'])
+end
+
+puts "Finished!"
